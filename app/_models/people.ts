c0a3b0f3c2c62e039983d9f087aaff1ed9f60164
Tim <tsbkwd@mst.edu>
2018-01-17 12:38:03 -0600

    added new fields to people and added matching fields to add user

--- app/_models/people.ts
+++ app/_models/people.ts
@@ -4,9 +4,20 @@
     lastname: string;
     address1: string;
     address2: string;
-    zipcode: number;
+    city: string;
     state: string;
-    phonenumber: string;
+    zipcode: number;
     email: string;
+    phonenumber: string;
+    skill1:string;
+    skill2:string;
+    skill3:string;
+    company: string;
     notes: string;
+    type:string; 
+    poc:string;
+    yearsexp:number;
+    payrate:number;
+    billrate:number;
+    availabledate:Date;
 }
